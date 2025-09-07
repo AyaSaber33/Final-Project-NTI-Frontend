4791a40320440f8e5a1644439f37f3015635bbf2
Aya Saber <aya.sabercollege@gmail.com>
2025-09-07 09:04:47 +0300

    finish page

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el))
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel(
+    {
+        items : 5,
+        nav : true,
+        navText : ['<i class="fa-solid fa-chevron-left"></i>' , '<i class="fa-solid fa-chevron-right"></i>'],
+        autoplay : true,
+        autoplayTimeout : 3000,
+        autoplayHoverPause : true,
+        rewind : true,
+    }
+  );
+});
