fa7b46e1e3b443bd61e20b2343a19447a5f9c4c0
Aya Saber <aya.sabercollege@gmail.com>
2025-09-07 10:11:42 +0300

    start js

---
+++ js/validate.js
@@ -0,0 +1,11 @@
+let userName = document.getElementById("userName")
+let email = document.getElementById("email")
+let subject = document.getElementById("subject")
+let Message = document.getElementById("Message")
+
+let userError = document.getElementById("userError")
+let emailError = document.getElementById("emailError")
+let subjectError = document.getElementById("subjectError")
+let messageError = document.getElementById("userError")
+let formError = document.getElementById("formError")
+
